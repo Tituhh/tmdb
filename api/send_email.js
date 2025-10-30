1989bfcda849af244bfb3ed4944dbc65295b2f50
Tituhh <sarvesh.asatkarr@gmail.com>
2025-10-30 08:45:31 +0530

    Upload send_email.js

---
+++ api/send_email.js
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
+
+  const { title, html, receiver } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: receiver,
+      subject: `🎬 Blogger Post: ${title}`,
+      html
+    });
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+}
